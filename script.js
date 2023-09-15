6c0702a2a4c00fb01ce7a3baace0cbb5f3aa5ed3
polnareff78 <nassim.tabari@gmail.com>
2023-09-15 15:00:57 +0200

    clearStorage

--- script.js
+++ script.js
@@ -71,6 +71,8 @@ function data() {
                     
                     myUniqueGraphDonutFunction();
                     sendEmail(formData);
+                    localStorage.clear();
+
                 }
 
                  if (this.step < this.maxStep) {
